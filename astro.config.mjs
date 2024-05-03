17c746c2425cf2508d7257f440716d37857e5b57
matiaslascano <matias_lascano@hotmail.com>
2024-05-03 10:05:30 -0300

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://matiaslascano.github.io',
+  site: 'https://matiaslascano.github.io/',
   base: 'cafeteria',
 });
